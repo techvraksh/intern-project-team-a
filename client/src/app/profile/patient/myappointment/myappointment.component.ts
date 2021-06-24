4aa2456696be44db276e1d27f36b0c63f3fa9fb2
Sahil Agarwal <agarwalsahil97@gmail.com>
2021-06-24 09:28:50 +0530

    adding patient components in profile folder

---
+++ client/src/app/profile/patient/myappointment/myappointment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myappointment',
+  templateUrl: './myappointment.component.html',
+  styleUrls: ['./myappointment.component.css']
+})
+export class MyappointmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
